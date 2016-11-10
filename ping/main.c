94bc2f1c66c7e74a3fa63f102fad73bac980096c
Mowgli <jean.stephane.thib@gmail.com>
2016-11-10 14:16:24 +0100

    first commit

---
+++ ping/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ping
+//
+//  Created by Jean-Stephane Mowgli Thibault  on 10/11/2016.
+//  Copyright © 2016 Jean-Stephane Mowgli Thibault . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
